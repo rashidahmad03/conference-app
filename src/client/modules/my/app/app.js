58878bf14ac3200b286e2997ef03899b1e1921a2
rashidahmad03 <rashidahmad2135@gmail.com>
2019-11-24 01:15:51 +0530

    first commit

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -7,21 +7,21 @@ export default class App extends LightningElement {
         this.sessionId = event.detail;
         this.state = 'details';
         window.history.pushState('details', null);
-      }
+    }
     constructor() {
         super();
         this.state = 'list';
         window.history.replaceState('list', null, '');
         window.onpopstate = event => {
-          if (event.state) {
-            this.state = event.state;
-          }
+            if (event.state) {
+                this.state = event.state;
+            }
         };
-      }
-      get isStateList() {
+    }
+    get isStateList() {
         return this.state === 'list';
-      }
-      get isStateDetails() {
+    }
+    get isStateDetails() {
         return this.state === 'details';
-      }
+    }
 }
